b4d8a2559efc287fb8259623d7ad9cea4bf7d76a
sebeomlee <leetmdrnjs77@gmail.com>
2024-01-25 12:42:22 +0900

    오타수정

--- farmworld_new/src/main/java/com/farmworld/farm/controller/MyFarmController.java
+++ farmworld_new/src/main/java/com/farmworld/farm/controller/MyFarmController.java
@@ -1,6 +1,5 @@
 package com.farmworld.farm.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,11 +8,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +18,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,7 +35,6 @@ import com.farmworld.farm.domain.MyFarmVO;
 import com.farmworld.farm.service.GrowUp;
 import com.farmworld.farm.service.MyFarm;
 import com.farmworld.shop.domain.GoodsVO;
-import com.google.gson.JsonObject;
 
 import lombok.AllArgsConstructor;
 
