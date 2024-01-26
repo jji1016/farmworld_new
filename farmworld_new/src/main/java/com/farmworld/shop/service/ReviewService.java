bfe1e75ac3f675d7e70a24996d347715e6fdecd2
KEDU_JY <zzazza1226@gmail.com>
2024-01-26 15:53:40 +0900

    지영-리뷰 디자인 수정

---
+++ farmworld_new/src/main/java/com/farmworld/shop/service/ReviewService.java
@@ -0,0 +1,8 @@
+package com.farmworld.shop.service;
+
+import com.farmworld.all.service.Generic;
+import com.farmworld.shop.domain.ReviewVO;
+
+public interface ReviewService extends Generic<ReviewVO, Integer>{
+	
+}
